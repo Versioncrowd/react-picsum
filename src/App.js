40c4ad5b8ac20e3136fbb1005ba87a6fde572991
Sabine Gottfried <sabine.gottfried@gmail.com>
2018-02-28 15:40:45 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      url: null,
+      author: null,
+      title: null,
+      date: null
+
+    }
+  }
+
+  getPicsumImage = (event) => {
+    
+  }
+
+  render() {
+    return (
+      <div className="App">  
+      <button onClick={this.getPicsumImage}>Get Random Picsum Image</button>
+      <div className="picsumreturn">
+        <h3>Author: {this.state.author}, Image title: {this.state.title}, Date shot: {this.state.date} </h3>
+        <img src={this.state.url} alt="random image" />
+      </div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
